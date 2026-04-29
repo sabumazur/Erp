8de23acecc0948afee2d1c9bb36bce7751edc80a
sabumazur <151460220+sabumazur@users.noreply.github.com>
2026-04-28 20:57:45 -0400

    fix: update email settings to use SSL and correct port configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,8 +19,10 @@ DATABASES = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=config("EMAIL_HOST_USER"))
